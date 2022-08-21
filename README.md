2219db5a8bd03a25a828cdfe2e52a2d3a97d0578
Phil Robinson <philip.robinson84@gmail.com>
2022-08-21 11:20:32 +0100

    Initial working version

---
+++ README.md
@@ -0,0 +1,60 @@
+# home-assistant-htu31d
+
+A simple Python app that reads temperature and humidity values from HTU31D I2C sensor and reports to an MQTT broker in a format compatible with Home Assistant.
+
+On initialisation a message is published to the config MQTT topic to add the sensors to Home Assistant using the MQTT Discovery service.
+
+The sensor name added to Home Assistant is the Host Name of the sensor, typically a Raspberry Pi.
+
+## Hardware Setup
+
+This is expected to run on a Raspberry Pi - tested on both the Pi4 and Pi2.
+
+Connect the Adafruit HTU31D sensor to I2C bus 1.
+
+Enable I2C using raspi-config.
+
+## Installation
+
+You'll need an MQTT broker, the code assumes it's on the same machine. In the code, change `localhost` to match the IP of the MQTT broker if this is not the case.
+
+Clone this repo.
+
+Install dependencies:
+
+```bash
+sudo apt-get install -y i2c-tools libgpiod-dev
+```
+
+Install python dependencies:
+
+```bash
+pip3 install -r requirements.txt
+```
+
+Copy service file:
+```bash
+sudo cp ./home-assistant-htu31d.service /lib/systemd/system/
+```
+
+Reload the system manager:
+```bash
+sudo systemctl daemon-reload
+```
+
+Enable the service:
+```bash
+sudo systemctl enable home-assistant-htu31d.service
+```
+
+Start the service
+```bash
+sudo systemctl start home-assistant-htu31d.service
+```
+
+Check status:
+```bash
+sudo systemctl status home-assistant-htu31d.service
+```
+
+As long as Home Assistant is already integrated wih your MQTT broker, new sensors as entities: `sensor.<HOSTNAME>_temperature_htu31d` and `sensor.<HOSTNAME>_humidity`
---
+++ home-assistant-htu31d.py
@@ -0,0 +1,38 @@
+import paho.mqtt.client as mqtt 
+import socket
+import json
+from time import sleep
+import board
+import adafruit_htu31d
+
+mqttBroker ="192.168.1.100" 
+room = socket.gethostname()
+
+client = mqtt.Client(f"{room} Room Temperature")
+client.connect(mqttBroker)
+client.loop_start()
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+htu = adafruit_htu31d.HTU31D(i2c)
+
+def registerSensors():
+    # temp
+    config = {"name": f"{room} temperature (HTU31D)", "device_class": "temperature", "unit_of_measurement": "°C", "state_topic": f"homeassistant/sensor/{room.lower()}/HTU31D_temperature", "unique_id": f"{room.lower()}_HTU31D_temperature"}
+    print(f"Registering sensor: {config}")
+    client.publish(f"homeassistant/sensor/{room.lower()}/HTU31D_temperature/config", json.dumps(config))
+    # humidity
+    config = {"name": f"{room} humidity", "device_class": "humidity", "unit_of_measurement": "%", "state_topic": f"homeassistant/sensor/{room.lower()}/HTU31D_humidity", "unique_id": f"{room.lower()}_HTU31D_humidity"}
+    print(f"Registering sensor: {config}")
+    client.publish(f"homeassistant/sensor/{room.lower()}/HTU31D_humidity/config", json.dumps(config))
+
+registerSensors()
+loopCount = 0
+while True:
+    loopCount += 1
+    if loopCount % 12 == 0:
+        registerSensors()
+    temperature, humidity = htu.measurements
+    print(f"HTU31D: Temperature: {round(temperature, 1)}, Humidity: {round(humidity, 1)}")
+    client.publish(f"homeassistant/sensor/{room.lower()}/HTU31D_temperature", round(temperature, 1))
+    client.publish(f"homeassistant/sensor/{room.lower()}/HTU31D_humidity", round(humidity, 1))
+    sleep(5)
